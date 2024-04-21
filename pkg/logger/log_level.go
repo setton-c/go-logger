f04c74f1515f0c4fdf323ed4cbfdbb5274f12393
setton-c <d.kiknadze@proton.me>
2024-04-21 11:41:22 +0400

    added a log level String() function and reworked the loggin system

--- pkg/logger/log_level.go
+++ pkg/logger/log_level.go
@@ -7,3 +7,16 @@ const (
 	WARNING
 	ERROR
 )
+
+func (l Log_level) String() string {
+	switch l {
+	case INFO:
+		return "info"
+	case WARNING:
+		return "warning"
+	case ERROR:
+		return "error"
+	default:
+		return "unknown"
+	}
+}
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,8 +1,8 @@
 package logger
 
 import (
-	"errors"
 	"fmt"
+	"strings"
 )
 
 type Logger struct {
@@ -17,33 +17,32 @@ func (lgr *Logger) Set_log_level(log_level Log_level) {
 	lgr.log_level = log_level
 }
 
-func (lgr *Logger) Log(message string) error {
-	if len(message) == 0 {
-		return errors.New("Message is Empty")
-	}
-	fmt.Println(message)
-
-	return nil
+func (lgr Logger) Log(level Log_level, args ...interface{}) {
+	level_str := strings.ToUpper(level.String())
+	
+	fmt.Print("["+level_str+"]: ")
+	fmt.Println(args...)
 }
 
-func (lgr *Logger) Info(msg string) {
-	if lgr.log_level <= INFO {
-		err := lgr.Log("[INFO] " + msg)
-		if err != nil {
-			lgr.Error(err.Error())
-			return
-		}
-	}
+func (lgr Logger) Logf(level Log_level, format_string string, args ...interface{}) {	
+	level_str := strings.ToUpper(level.String())
+	fmt.Printf("["+level_str+"]: " + format_string + "\n", args...)
 }
 
-func (lgr *Logger) Warning(msg string) {
-	if lgr.log_level <= WARNING {
-		lgr.Log("[WARNING] " + msg)
-	}
-}
-
-func (lgr *Logger) Error(msg string) {
-	if lgr.log_level <= ERROR {
-		lgr.Log("[ERROR] " + msg)
-	}
-}
+// func (lgr *Logger) Info(msg string) {
+// 	if lgr.log_level <= INFO {
+// 		lgr.Log("[INFO] " + msg)
+// 	}
+// }
+//
+// func (lgr *Logger) Warning(msg string) {
+// 	if lgr.log_level <= WARNING {
+// 		lgr.Log("[WARNING] " + msg)
+// 	}
+// }
+//
+// func (lgr *Logger) Error(msg string) {
+// 	if lgr.log_level <= ERROR {
+// 		lgr.Log("[ERROR] " + msg)
+// 	}
+// }
