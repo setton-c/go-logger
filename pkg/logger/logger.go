7e96dd62b252df1d57ebd734e5d774cc7ed1c4df
setton-c <d.kiknadze@proton.me>
2024-04-21 16:10:39 +0400

    update the way logging functions work

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -17,32 +17,32 @@ func (lgr *Logger) Set_log_level(log_level Log_level) {
 	lgr.log_level = log_level
 }
 
-func (lgr Logger) Log(level Log_level, args ...interface{}) {
-	level_str := strings.ToUpper(level.String())
+func (lgr Logger) Log(args ...interface{}) {
+	level_str := strings.ToUpper(lgr.log_level.String())
 	
 	fmt.Print("["+level_str+"]: ")
 	fmt.Println(args...)
 }
 
-func (lgr Logger) Logf(level Log_level, format_string string, args ...interface{}) {	
-	level_str := strings.ToUpper(level.String())
+func (lgr Logger) Logf(format_string string, args ...interface{}) {	
+	level_str := strings.ToUpper(lgr.log_level.String())
 	fmt.Printf("["+level_str+"]: " + format_string + "\n", args...)
 }
 
-// func (lgr *Logger) Info(msg string) {
-// 	if lgr.log_level <= INFO {
-// 		lgr.Log("[INFO] " + msg)
-// 	}
-// }
-//
-// func (lgr *Logger) Warning(msg string) {
-// 	if lgr.log_level <= WARNING {
-// 		lgr.Log("[WARNING] " + msg)
-// 	}
-// }
-//
-// func (lgr *Logger) Error(msg string) {
-// 	if lgr.log_level <= ERROR {
-// 		lgr.Log("[ERROR] " + msg)
-// 	}
-// }
+func (lgr *Logger) Info(msg string) {
+	if lgr.log_level <= INFO {
+		lgr.Log(msg)
+	}
+}
+
+func (lgr *Logger) Warning(msg string) {
+	if lgr.log_level <= WARNING {
+		lgr.Log( msg)
+	}
+}
+
+func (lgr *Logger) Error(msg string) {
+	if lgr.log_level <= ERROR {
+		lgr.Log(msg)
+	}
+}
